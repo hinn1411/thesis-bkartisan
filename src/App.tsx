add606b08dd71020577311e2ffae65e5d9f5356d
Hien Giang <quachtrieuvu@gmail.com>
2023-11-02 21:35:08 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+
+  return (
+    <div className='bg-cyan-700'>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
