9222069faceac9bd6482129b4842e648a651916b
Hoi Lau <lauhoi2010@gmail.com>
2023-11-08 22:47:23 +0700

    Add backend

---
+++ backend/src/controllers/user.ts
@@ -0,0 +1,20 @@
+import passport from "passport";
+
+import User from "../models/User.js";
+import { hashPassword } from "../utils/helpers.js";
+
+export const postLogin = (req, res, next) => {
+  passport.authenticate("local")(req, res, next);
+};
+
+export const postRegister = async (request, response, next) => {
+  const { username, email } = request.body;
+  const userDB = await User.findOne({ username });
+  if (userDB) {
+    response.status(400).send({ msg: "User already exists!" });
+  } else {
+    const password = hashPassword(request.body.password);
+    const newUser = await User.create({ username, password, email });
+    response.send(201);
+  }
+};
