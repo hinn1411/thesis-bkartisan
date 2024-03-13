35752ab90f52e64d03ff7aa9f81d1e998ba78bbe
Hien Giang <quachtrieuvu@gmail.com>
2024-03-13 20:32:23 +0700

    refactor fetch logic with custom hook & delete old backend

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,59 +1,25 @@
-import { FC, memo, useState } from 'react';
+import { FC, memo } from 'react';
 import CategoryCard from '../../components/common/category/CategoryCard';
 // Product images
 import ProductCard, {
   ProductCardProps,
 } from '../../components/common/product/ProductCard';
 import Pagination from '../../components/common/pagination/Pagination';
+import { useProductPagination } from './hooks/useProductPagination';
 import { useTranslation } from 'react-i18next';
 import { categoryData } from './data';
-import { useQuery } from '@tanstack/react-query';
-import apiProducts from '../../apis/apiProducts';
-const PAGE = 1,
-  OFFSET = 1;
+
 const Home: FC = memo(() => {
   console.log(`render home`);
-
   const { t } = useTranslation();
-
-  const [currentPage, setCurrentPage] = useState(PAGE);
-  const goToNextPage = () => {
-    setCurrentPage((prev) => prev + 1);
-    console.log(`current page = ${currentPage}`);
-    // refetch();
-  };
-  const goToPage = (newPage: number) => {
-    setCurrentPage(prev => newPage);
-  }
-  const goToPreviousPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage((prev) => prev - 1);
-    }
-    console.log(`current page = ${currentPage}`);
-    // refetch();
-  };
-  // console.log(categoryData);
-  const {
-    data: products,
-    isSuccess,
-    refetch,
-  } = useQuery({
-    queryKey: ['products', currentPage],
-    // notifyOnChangeProps: 'all',
-    queryFn: async () => {
-      console.log(`call api, ${currentPage}`);
-
-      return await apiProducts.getProducts(currentPage, OFFSET);
-    },
-  });
-  // console.log(`products`, products);
+  const { data: products, page, setPage, isSuccess } = useProductPagination();
 
   return (
     <main className="min-h-screen px-20 my-5">
       <h1 className="text-3xl text-center font-medium">
         {t('category.title')}
       </h1>
-      {/* Image container */}
+      {/* Category list */}
       <div className="flex flex-col md:flex-row items-center justify-evenly text-base space-x-2 my-8 border-b-2 pb-8 border-b-gray-300">
         {categoryData.map((category) => (
           <CategoryCard
@@ -62,27 +28,6 @@ const Home: FC = memo(() => {
             categoryName={t(category.categoryName)}
           />
         ))}
-        {/* <CategoryCard
-          srcImage={cateImage1}
-          categoryName={t('category.anniversary')}
-        />
-        <CategoryCard
-          srcImage={cateImage2}
-          categoryName={t('category.women')}
-        />
-        <CategoryCard srcImage={cateImage3} categoryName={t('category.men')} />
-        <CategoryCard
-          srcImage={cateImage4}
-          categoryName={t('category.children')}
-        />
-        <CategoryCard
-          srcImage={cateImage5}
-          categoryName={t('category.family')}
-        />
-        // <CategoryCard
-        //   srcImage={cateImage6}
-        //   categoryName={t('category.unique')}
-        // /> */}
       </div>
       {/* Product list */}
       <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
@@ -91,13 +36,7 @@ const Home: FC = memo(() => {
             <ProductCard key={index} {...product} />
           ))}
       </div>
-
-      <Pagination
-        currentPage={PAGE}
-        goToLeft={goToPreviousPage}
-        goToRight={goToNextPage}
-        goToPage={goToPage}
-      />
+      <Pagination currentPage={page} goToPage={setPage} />
     </main>
   );
 });
