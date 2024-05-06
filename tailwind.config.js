b6d3dfbfc26215fa7cb6db0c21ad20718f47fbcc
Hoi Lau <lauhoi2010@gmail.com>
2024-05-06 21:46:42 +0700

    Fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-<<<<<<< HEAD
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,css}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/flowbite/**/*.js",
-  ],
-  theme: {
-    fontFamily: {
-      sans: ["Poppins", "sans-serif"],
-=======
     './index.html',
     './src/**/*.{js,ts,jsx,tsx,css}',
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
@@ -19,16 +9,11 @@ export default {
   theme: {
     fontFamily: {
       sans: ['Be Vietnam Pro', 'Poppins', 'sans-serif'],
->>>>>>> master
     },
     extend: {},
   },
   plugins: [
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-<<<<<<< HEAD
-    require("flowbite/plugin"),
-=======
     require('flowbite/plugin'),
->>>>>>> master
   ],
 };
