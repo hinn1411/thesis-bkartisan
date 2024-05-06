4640ffd45a797b8f0a1883b2b1dc695f284318a1
Hoi Lau <lauhoi2010@gmail.com>
2024-05-06 21:27:31 +0700

    Solve merge conflict

--- src/pages/Admin/Report/OrderReport.tsx
+++ src/pages/Admin/Report/OrderReport.tsx
@@ -4,8 +4,13 @@ import ReturnIcon from "../../../components/admin/ReturnIcon";
 import { TextInput, Button, Select } from "flowbite-react";
 import TextField from "../../../components/admin/TextField";
 import { BsChatLeftDots } from "react-icons/bs";
+import { formatDate } from "../../../utils/formatDate";
 
-const OrderReport: FC = memo(() => {
+interface OrderReportProps {
+  report: any
+}
+
+const OrderReport: FC<OrderReportProps> = memo(({report}) => {
 
   return (
     <Box paddingBottom={4}>
@@ -14,21 +19,25 @@ const OrderReport: FC = memo(() => {
         <ReturnIcon />
       </Box>
       <Grid container p={1} columnGap={9} rowGap={3} paddingBottom={2}>
-        <Grid item xs={3.5}>
+      <Grid item xs={3.5}>
           <Box className="font-medium pb-2">Người bị report</Box>
-          <TextField value="Lầu Hội" />
+          <TextField value={report.reportedUser} />
         </Grid>
         <Grid item xs={3.5}>
           <Box className="font-medium pb-2">Người gửi</Box>
-          <TextField value="Lầu Hội" />
+          <TextField value={report.reporter} />
         </Grid>
         <Grid item xs={3.5}>
           <Box className="font-medium pb-2">Thời gian gửi</Box>
-          <TextField value="Lầu Hội" />
+          <TextField value={formatDate("hh:MM dd/mm/yyyy", new Date(report.createdAt))} />
         </Grid>
         <Grid item xs={12}>
           <Box className="font-medium pb-2">Lí do vi phạm</Box>
-          <TextField value="Lầu Hội" />
+          <TextField value={report.reason} />
+        </Grid>
+        <Grid item xs={12}>
+          <Box className="font-medium pb-2">Thông tin thêm từ người report</Box>
+          <TextField textarea value={report.additionalInfo} minHeight="min-h-28" />
         </Grid>
         <Grid item xs={6} display="flex" justifyContent="space-between">
           <Button color="light"><BsChatLeftDots className="mr-2 h-5 w-5"/> Nhắn tin với người bị report</Button>
--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,13 +1,33 @@
-type DateFormat = "dd/mm/yyyy" | "dd-mm-yyyy" | "yyyy-mm-dd"
+type DateFormat =
+  | "dd/mm/yyyy"
+  | "dd-mm-yyyy"
+  | "yyyy-mm-dd"
+  | "hh:MM dd/mm/yyyy";
 
 export function formatDate(format: DateFormat, date: Date) {
-    if (format === "dd/mm/yyyy") {
-        return date.toLocaleDateString("vi-VN");
-    }
-    else if (format === "yyyy-mm-dd") {
-        return date.getFullYear()+ "-" + (date.getMonth() + 1) + "-" +  date.getDate(); 
-    }
-    else {
-        return date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear(); 
-    }
+  if (format === "dd/mm/yyyy") {
+    return (
+      date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()
+    );
+  } else if (format === "hh:MM dd/mm/yyyy") {
+    return (
+      date.getHours() +
+      ":" +
+      date.getMinutes() +
+      (date.getHours() < 12 ? "AM " : "PM ") +
+      date.getDate() +
+      "/" +
+      (date.getMonth() + 1) +
+      "/" +
+      date.getFullYear()
+    );
+  } else if (format === "yyyy-mm-dd") {
+    return (
+      date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()
+    );
+  } else {
+    return (
+      date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear()
+    );
+  }
 }
