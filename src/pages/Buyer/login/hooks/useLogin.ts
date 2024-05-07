2c4b56ec6805ad0f5ee273a6af6560023f7eb9f2
Hien Giang <quachtrieuvu@gmail.com>
2024-05-07 19:18:04 +0700

    update: route to admin & seller page

--- src/pages/Buyer/login/hooks/useLogin.ts
+++ src/pages/Buyer/login/hooks/useLogin.ts
@@ -1,16 +1,25 @@
 import { useMutation } from '@tanstack/react-query';
 import apiAuth from '@apis/apiAuth';
 import { useNavigate } from 'react-router-dom';
+import { USERS } from '@contants/users';
 export const useLogin = () => {
   const navigate = useNavigate();
   const { mutate, isPending, isSuccess, isError, error } = useMutation({
     mutationFn: async (userData) => {
       const { data } = await apiAuth.login(userData);
-      console.log(`user login info = `, data);
+      // console.log(`user login info = `, data);
       return data;
     },
-    onSuccess: () => {
-      navigate('/');
+    onSuccess: (userInfo) => {
+      const { role } = userInfo.data;
+      console.log(userInfo);
+      if (role === USERS.ADMIN) {
+        return navigate('/admin');
+      }
+      if (role === USERS.SELLER) {
+        return navigate('/seller');
+      }
+      return navigate('/');
     },
   });
   return {
