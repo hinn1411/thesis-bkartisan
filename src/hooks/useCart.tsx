7bd28bca660c27fc3d5c6fcc2ff078b635780770
Hien Giang <quachtrieuvu@gmail.com>
2024-05-13 15:49:37 +0700

    feat: added apply discount feature

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,22 +1,31 @@
 import apiCart from '@apis/apiCart';
-import { ErrorIcon, Message, SuccessIcon, options } from '@components/common/toast';
+import {
+  ErrorIcon,
+  Message,
+  SuccessIcon,
+  options,
+} from '@components/common/toast';
 import { useMutation } from '@tanstack/react-query';
+import { useContext } from 'react';
 import { ToastOptions, toast } from 'react-toastify';
+import { CartContext, CartContextType } from 'src/store/cartContext';
 export const useCart = () => {
+  const { updateNumberOfItems } = useContext(CartContext) as CartContextType;
   const { mutate } = useMutation({
     mutationFn: apiCart.addToCart,
     onSuccess: () => {
+      updateNumberOfItems(1);
       toast.success(<Message>Thêm thành công</Message>, {
         icon: <SuccessIcon />,
-        ...(options as ToastOptions)
-      })
+        ...(options as ToastOptions),
+      });
     },
     onError: () => {
       toast.error(<Message>Vui lòng thử lại</Message>, {
         icon: <ErrorIcon />,
         ...(options as ToastOptions),
       });
-    }
+    },
   });
   return {
     addToCart: mutate,
--- src/pages/Buyer/cart/Cart.tsx
+++ src/pages/Buyer/cart/Cart.tsx
@@ -1,33 +1,55 @@
-import { FC, memo } from 'react';
+import { ChangeEvent, FC, memo, useContext, useEffect } from 'react';
 import momoImage from '@images/cart/momo.png';
 import { useCart } from './hooks/useFetchCart';
 import ItemList from './components/ItemList';
+import { DiscountProps, useDiscount } from './hooks/useDiscount';
+import { formatCurrency } from '@utils/formatCurrency';
+import { CURRENCIES } from '@contants/currencies';
+import { useHandleDiscount } from './hooks/useHandleDiscount';
+import { CartContext, CartContextType } from 'src/store/cartContext';
 const Cart: FC = memo(() => {
-  const { cart, isFetching, numOfItems } = useCart();
+  const { numberOfItems, originalPrice, discountPrice } = useContext(
+    CartContext
+  ) as CartContextType;
+  const { cart, isFetching } = useCart();
+  const { discount, setDiscount, discountList, applyDiscount } = useDiscount();
+  const { clearDiscount } = useHandleDiscount();
 
-  const orderPrice = new Intl.NumberFormat('vi-VN', {
-    style: 'currency',
-    currency: 'VND',
-  }).format(2000000);
-  const discountPrice = new Intl.NumberFormat('vi-VN', {
-    style: 'currency',
-    currency: 'VND',
-  }).format(100000);
-  const subTotal = new Intl.NumberFormat('vi-VN', {
-    style: 'currency',
-    currency: 'VND',
-  }).format(1900000);
-  const shippingPrice = new Intl.NumberFormat('vi-VN', {
-    style: 'currency',
-    currency: 'VND',
-  }).format(50000);
-  const totalPrice = new Intl.NumberFormat('vi-VN', {
-    style: 'currency',
-    currency: 'VND',
-  }).format(1950000);
+  useEffect(() => {
+    window.onbeforeunload = () => {
+      clearDiscount();
+    };
+    return () => {
+      window.onbeforeunload = null;
+    };
+  });
+  const handleChangeDiscount = (e: ChangeEvent<HTMLInputElement>) => {
+    const newDiscountVal = e.target.value;
+    setDiscount((prev) => newDiscountVal);
+  };
+  const handleUseDiscount = () => {
+    if (!discount) {
+      return;
+    }
+    applyDiscount(discount);
+  };
+
+  const subTotal = formatCurrency(
+    originalPrice - discountPrice,
+    CURRENCIES.VIETNAMDONG
+  );
+  // shipping cost is missing from BE
+  const orderPrice = formatCurrency(
+    originalPrice - discountPrice - 0,
+    CURRENCIES.VIETNAMDONG
+  );
+  // const totalPrice = new Intl.NumberFormat('vi-VN', {
+  //   style: 'currency',
+  //   currency: 'VND',
+  // }).format(1950000);
   return (
     <div className="min-h-screen px-4 md:px-20 my-5">
-      <h1 className="text-2xl my-3">Tổng số sản phẩm: {numOfItems}</h1>
+      <h1 className="text-2xl my-3">Tổng số sản phẩm: {numberOfItems}</h1>
       <div className="flex flex-col sm:flex-row justify-between space-y-8 md:space-y-0 md:space-x-6">
         {/* Items container */}
         <ItemList
@@ -111,11 +133,18 @@ const Cart: FC = memo(() => {
           <div className="w-full mt-6">
             <div className="flex items-center justify-between">
               <p>Tổng đơn hàng</p>
-              <p>{orderPrice}</p>
+              <p>{formatCurrency(originalPrice, CURRENCIES.VIETNAMDONG)}</p>
             </div>
-            <div className="flex items-center justify-between">
+            <div className="">
               <p>Giảm giá</p>
-              <p>{discountPrice}</p>
+              {discountList.map((discount: DiscountProps) => (
+                <div className="text-sm flex justify-between">
+                  <p>#{discount.code}</p>
+                  <p>
+                    -{formatCurrency(discount.value, CURRENCIES.VIETNAMDONG)}
+                  </p>
+                </div>
+              ))}
             </div>
             <hr className="my-2 h-0.5 border-t-0 bg-gray-300 opacity-100 dark:opacity-50" />
             <div className="flex items-center justify-between">
@@ -124,12 +153,14 @@ const Cart: FC = memo(() => {
             </div>
             <div className="flex items-center justify-between">
               <p>Phí vận chuyển</p>
-              <p>{shippingPrice}</p>
+              <p>{formatCurrency(0, CURRENCIES.VIETNAMDONG)}</p>
             </div>
             <hr className="my-2 h-0.5 border-t-0 bg-gray-300 opacity-100 dark:opacity-50" />
             <div className="flex items-center justify-between">
               <p>Tổng tiền</p>
-              <p>{totalPrice}</p>
+              <p>
+               {orderPrice}
+              </p>
             </div>
             {/* Button container */}
             <div className="flex flex-col space-y-3 my-4">
@@ -138,10 +169,15 @@ const Cart: FC = memo(() => {
               </button>
               <div className="flex py-2">
                 <input
+                  onChange={handleChangeDiscount}
+                  value={discount}
                   className="border border-black rounded-l-full w-2/3 py-2 px-2.5 text-sm border-opacity-50"
                   placeholder="Nhập mã"
                 />
-                <button className="bg-black text-white text-center w-1/3 rounded-r-full">
+                <button
+                  onClick={handleUseDiscount}
+                  className="bg-black text-white text-center w-1/3 rounded-r-full"
+                >
                   Áp dụng
                 </button>
               </div>
