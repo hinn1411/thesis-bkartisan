6d8b29b07cf4cf867c72d336e5ca9c71331d555d
Hien Giang <quachtrieuvu@gmail.com>
2024-07-04 17:12:06 +0700

    change vite base url

--- vite.config.ts
+++ vite.config.ts
@@ -18,10 +18,10 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   // test: vitestConfig.test,
   plugins: [react({}), tsconfigPaths()],
-  base: "/",
+  base: "/thesis-bkartisan/",
   preview: {
-    port: 5173
-  }
+    port: 5173,
+  },
   // resolve: {npm install --save-dev
   //   alias: [
   //     {
